d76629e8cd564ca9f5135591abed98dfddad8792
abduvik <contact@abdu.dev>
2022-07-02 22:45:57 +0200

    Create project start

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wp-shopify-clone
 Shopify-Clone SaaS using WordPress and WPCS.io
+
+Start Docker compose
+
+`docker-compose up`
+
+Create symlink
+
+`ln -s "$(pwd)/src/" dist/plugins/wpcs-woo-subscriptions`
---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WPCSWooSubscriptions
+ * @version 1.0.0
+ */
+/*
+Plugin Name: WPCS WooCommerce Subscriptions
+Plugin URI: https://github.com/abduvik/wp-shopify-clone
+Description: This plugin is used to create tenants on WPCS.io with support of WordPress, WooCommerce and YITH Subscriptions
+Author: Abdu Tawfik
+Version: 1.0.0
+Author URI: https://www.abdu.dev
+*/
